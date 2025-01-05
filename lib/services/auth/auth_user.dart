e63214c8ed40bd0324bb70c500fcb8d67afd153e
) <nasserabudalbouh@gmail.com>
2025-01-05 15:13:43 +0300

    Implemented Service and Provider Layers

---
+++ lib/services/auth/auth_user.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart' as FirebaseAuth show User;
+import 'package:flutter/material.dart';
+
+@immutable
+class AuthUser {
+  final bool isEmailVerified;
+  const AuthUser(this.isEmailVerified);
+
+  factory AuthUser.fromFirebase(FirebaseAuth.User user) {
+    return AuthUser(user.emailVerified);
+  }
+}
